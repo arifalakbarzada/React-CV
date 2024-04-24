45473ce62fea66797db87178ac7c28a30da86072
arif2004js <yearif689@gmail.com>
2024-04-24 16:58:20 +0400

    Cv created

---
+++ src/Cv.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function CV() {
+    return (
+        <div className="cv-container">
+            <div className="header">
+                <h1>Arif Alakbarzada</h1>
+                <p>Front-End Developer</p>
+            </div>
+            <div className="section">
+                <h2>Education</h2>
+                <p>Study for Computer Engineering - ASOIU University</p>
+            </div>
+            <div className="section">
+                <h2>Experience</h2>
+                <p>Front-End Mentor at IT Brains Academy(2024-now)</p>
+            </div>
+            <div className="section">
+                <h2>Skills</h2>
+                <p>React, JavaScript, HTML, CSS, Bootstrap , SCSS</p>
+            </div>
+        </div>
+    );
+}
+
+export default CV;
